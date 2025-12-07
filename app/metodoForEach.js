eb13eda6efe284949e007ba7006a6040d835a399
Ariel <160680453+azrlsli@users.noreply.github.com>
2025-12-07 18:12:57 -0300

    Adicionado comentario

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -1,14 +1,22 @@
+// Seleciona o elemento HTML onde os livros serão inseridos
 const elementoParaInserirLivros = document.getElementById('livros');
+
+// Seleciona o elemento onde será exibido o valor total dos livros disponíveis
 const elementoComValorTotalDeLivrosDisponiveis = document.getElementById('valor_total_livros_disponiveis');
 
+// Função responsável por exibir os livros na tela
 function exibirOsLivrosNaTela(listaDeLivros) {
+  // Limpa o valor total (caso tenha sido exibido antes)
   elementoComValorTotalDeLivrosDisponiveis.innerHTML = '';
+  // Limpa a área onde os livros serão exibidos
   elementoParaInserirLivros.innerHTML = '';
 
+    // Percorre a lista de livros recebida
     listaDeLivros.forEach(livro => {
       // let disponibilidade = verificarDisponibilidadeDoLivreo(livro);
-      let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';
+      let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel';  // Verifica se o livro está disponível (quantidade > 0)
 
+      // Insere na tela o HTML correspondente a cada livro
         elementoParaInserirLivros.innerHTML += `
             <div class="livro">
       <img class="${disponibilidade}" src="${livro.imagem}"
@@ -32,4 +40,5 @@ function exibirOsLivrosNaTela(listaDeLivros) {
 //   } else {
 //     return 'livro__imagens indisponivel'
 //   }
+
 // }
